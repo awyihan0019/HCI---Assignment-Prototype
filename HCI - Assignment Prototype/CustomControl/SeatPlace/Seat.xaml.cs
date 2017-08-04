dcc1e08418e829feeef137dab73580cd42792cbb
WJH <hou32hou@gmail.com>
2017-08-04 21:55:16 +0800

    Added seats icon for SeatPlace control

---
+++ HCI - Assignment Prototype/CustomControl/SeatPlace/Seat.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI___Assignment_Prototype.CustomControl.SeatPlace {
+    /// <summary>
+    /// Interaction logic for Seat.xaml
+    /// </summary>
+    public partial class Seat : UserControl {
+        public Seat() {
+            InitializeComponent();
+        }
+    }
+}
