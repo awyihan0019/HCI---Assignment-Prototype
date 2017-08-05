00d1175dd4bf1c5a6a7ee08124a49d8411d6af88
yihan <faster960128@hotmail.com>
2017-08-05 17:54:27 +0800

    Change some interface adding the view profile click

--- HCI - Assignment Prototype/Page/Homepage/Homepage_AfterLogin.xaml.cs
+++ HCI - Assignment Prototype/Page/Homepage/Homepage_AfterLogin.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using HCI___Assignment_Prototype.CustomControl;
+using HCI___Assignment_Prototype.Page.Profile_Page;
+using System.Windows.Controls;
 
 namespace HCI___Assignment_Prototype.Page.Homepage {
     /// <summary>
@@ -7,6 +9,16 @@ namespace HCI___Assignment_Prototype.Page.Homepage {
     public partial class Homepage_AfterLogin : UserControl {
         public Homepage_AfterLogin() {
             InitializeComponent();
+        }
+
+        private void ViewProfile(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MainWindow.MainFrame.Navigate(new UserControl_UserProfile());
+        }
+
+        private void LogOut(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DialogBox.Show("Logout the account", "Do you really want to logout your account?", "Cancel", "Logout");
         }
     }
 }
