2486360d63a4cea0af9b862288ef51dd08e76fd0
WJH <hou32hou@gmail.com>
2017-08-07 15:50:31 +0800

    Added ImageUri Property for Movie.cs

--- HCI - Assignment Prototype/Class/Movie.cs
+++ HCI - Assignment Prototype/Class/Movie.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace HCI___Assignment_Prototype.Class {
     public class Movie {
+        public string ImageUri { get; set; }
         public string Name { get; set; }
         public List<DateTime> Date { get; set; }
         public List<DateTime> Time { get; set; }
@@ -13,16 +14,18 @@ namespace HCI___Assignment_Prototype.Class {
         public List<string> AvailableLocation { get; set; }
         public string Category { get; set; }
 
-        public Movie(string name, List<DateTime> date, List<DateTime> time, string description, List<string> availableLocation, string category) {
+        public Movie(string name, List<DateTime> date, List<DateTime> time, string description, List<string> availableLocation, string category, string imageUri) {
             Name = name;
             Date = date;
             Time = time;
             Description = description;
             AvailableLocation = availableLocation;
             Category = category;
+            ImageUri = imageUri;
         }
 
-        public Movie() {
+        public Movie(string imageUri) {
+            ImageUri = imageUri;
         }
     }
 }
