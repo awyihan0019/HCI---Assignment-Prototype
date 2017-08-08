08a56ee794f8be68df5b883c581c3d3cd28430d6
WJH <hou32hou@gmail.com>
2017-08-08 22:35:43 +0800

    Fixed the functionality of DialogBox - now it can navigates content

--- HCI - Assignment Prototype/CustomControl/DialogBox.xaml.cs
+++ HCI - Assignment Prototype/CustomControl/DialogBox.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using HCI___Assignment_Prototype.Class;
 
 namespace HCI___Assignment_Prototype.CustomControl {
     /// <summary>
@@ -18,19 +21,21 @@ namespace HCI___Assignment_Prototype.CustomControl {
 
         public DialogBox() {
             InitializeComponent();
+            this.AllowsTransparency = true;
         }
 
-        public static ResultEnum Show(string title, string message, string leftButtonText = "Got it!",
+        public static ResultEnum Show(string title , string message , string leftButtonText = "Got it!" ,
                                       string rightButtonText = null) {
-            var p = new DialogBox();
-            p.SetContent(title, message, leftButtonText, rightButtonText);
+            if (_singleton == null) _singleton = new DialogBox();
+            var p = _singleton;
+            p.SetContent(title , message , leftButtonText , rightButtonText);
             p.DialogHost.IsOpen = true;
             p.ShowDialog();
             Result = p._result;
             return p._result;
         }
 
-        private void SetContent(string title, string message, string leftButtonText, string rightButtonText) {
+        private void SetContent(string title , string message , string leftButtonText , string rightButtonText) {
             Button_Right.Visibility = rightButtonText == null ? Visibility.Collapsed : Visibility.Visible;
             TextBlock_Title.Text = title;
             TextBlock_Message.Text = message;
@@ -40,32 +45,44 @@ namespace HCI___Assignment_Prototype.CustomControl {
             Default.Visibility = Visibility.Visible;
         }
 
+        private static DialogBox _singleton;
         public static void Show(UserControl content) {
-            var p = new DialogBox();
+            if (_singleton == null) _singleton = new DialogBox();
+            var p = _singleton;
             p.SetContent(content);
             p.DialogHost.IsOpen = true;
-            p.AllowsTransparency = true;
+            if (p.Visibility == Visibility.Visible) return;
             p.ShowDialog();
+        }
+
+        public static void CloseDialog() {
+            _singleton.Hide();
+        }
+
+        public static void GoBack() {
+            if (_singleton == null) return;
+            if (_singleton.Frame.CanGoBack == false) return;
+            _singleton.Frame.GoBack();
         }
 
         private void SetContent(UserControl content) {
             Custom.Visibility = Visibility.Visible;
             Default.Visibility = Visibility.Collapsed;
-            ContentPresenter.Content = content;
+            Frame.Navigate(content);
         }
 
-        private void Button_Left_OnClick(object sender, RoutedEventArgs e) {
+        private void Button_Left_OnClick(object sender , RoutedEventArgs e) {
             _result = ResultEnum.LeftButtonClicked;
             Hide();
         }
 
-        private void Button_Right_OnClick(object sender, RoutedEventArgs e) {
+        private void Button_Right_OnClick(object sender , RoutedEventArgs e) {
             _result = ResultEnum.RightButtonClicked;
             Hide();
         }
 
         private void SampleCode() {
-            Show("Title", "message", "OK", "Cancel");
+            Show("Title" , "message" , "OK" , "Cancel");
             switch (Result) {
                 case ResultEnum.LeftButtonClicked:
                     MessageBox.Show("You clicked left button");
@@ -74,6 +91,22 @@ namespace HCI___Assignment_Prototype.CustomControl {
                     MessageBox.Show("you clicked right button");
                     break;
             }
+        }
+
+        private void Frame_OnNavigating(object sender , NavigatingCancelEventArgs e) {
+            return;
+            var ta = new ThicknessAnimation {
+                Duration = CustomAnimation.FullScreenAnimationDuration ,
+                DecelerationRatio = CustomAnimation.DecelerationConstant ,
+                To = new Thickness(0 , 0 , 0 , 0)
+            };
+            if (e.NavigationMode == NavigationMode.New || e.NavigationMode == NavigationMode.Forward) {
+                ta.From = new Thickness(ActualWidth / 3 , 0 , 0 , 0);
+            }
+            else if (e.NavigationMode == NavigationMode.Back) {
+                ta.From = new Thickness(0 , 0 , ActualWidth / 3 , 0);
+            }
+                (e.Content as UIElement)?.BeginAnimation(MarginProperty , ta);
         }
     }
 }
