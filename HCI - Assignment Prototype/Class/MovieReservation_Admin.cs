bc53c1767e5b5b1d2ead3fdd6ece67498c76851d
WJH <hou32hou@gmail.com>
2017-08-08 21:52:39 +0800

    Added class MovieReservation_Admin.cs

---
+++ HCI - Assignment Prototype/Class/MovieReservation_Admin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI___Assignment_Prototype.Class {
+   public class MovieReservation_Admin {
+
+    }
+}
