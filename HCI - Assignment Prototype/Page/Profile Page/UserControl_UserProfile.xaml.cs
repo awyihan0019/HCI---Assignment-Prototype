0d351c014627c8bb4da7415851be7343ecdcbb55
Winston Ng <wincern@gmail.com>
2017-08-09 21:39:38 +0800

    Added some improvements to the designs of: - UserProfile.xaml Added new variables to: -UserProfile.xaml.cs -EditProfile.xaml.cs

--- HCI - Assignment Prototype/Page/Profile Page/UserControl_UserProfile.xaml.cs
+++ HCI - Assignment Prototype/Page/Profile Page/UserControl_UserProfile.xaml.cs
@@ -8,10 +8,18 @@ namespace HCI___Assignment_Prototype.Page.Profile_Page {
     public partial class UserControl_UserProfile : UserControl {
         public UserControl_UserProfile() {
             InitializeComponent();
+            var u = Global.Global.CurrentUser;
+            NameField.Text = u.Username;
+            PhoneField.Text = u.PhoneNumber;
+            AddressField.Text = u.Address;
+            EmailField.Text = u.Email;
+            ProfilePicture.Source = u.ProfilePicture;
         }
 
         private void EditProfile_Click(object sender, RoutedEventArgs e) {
             MainWindow.MainFrame.Navigate(new UserControl_EditProfile());
+
+            
         }
     }
 }
