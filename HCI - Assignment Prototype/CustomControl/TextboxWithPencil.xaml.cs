c9c81fc2f996b46b6449db4bade4e183083c9f1b
yihan <faster960128@hotmail.com>
2017-08-09 22:27:12 +0800

    Added new property for textboxwithpencil (isPassword)

--- HCI - Assignment Prototype/CustomControl/TextboxWithPencil.xaml.cs
+++ HCI - Assignment Prototype/CustomControl/TextboxWithPencil.xaml.cs
@@ -1,34 +1,44 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using HCI___Assignment_Prototype.Class;
 using static System.Windows.Visibility;
+using System.Security;
+using System.Text;
 
-namespace HCI___Assignment_Prototype.CustomControl {
+namespace HCI___Assignment_Prototype.CustomControl
+{
     /// <summary>
     ///     Interaction logic for TextboxWithPencil.xaml
     /// </summary>
-    public partial class TextboxWithPencil : UserControl {
-        public TextboxWithPencil() {
+    public partial class TextboxWithPencil : UserControl
+    {
+        public TextboxWithPencil()
+        {
             InitializeComponent();
         }
 
-        private void Button_Pencil_OnClick(object sender, RoutedEventArgs e) {
+        private void Button_Pencil_OnClick(object sender, RoutedEventArgs e)
+        {
             GoToEditableMode();
             Keyboard.Focus(TextBox);
         }
 
-        private void Button_Cancel_OnClick(object sender, RoutedEventArgs e) {
+        private void Button_Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
             TextBox.Text = TextBlock.Text;
             GoToUneditableMode();
         }
 
-        private void Button_Save_OnClick(object sender, RoutedEventArgs e) {
+        private void Button_Save_OnClick(object sender, RoutedEventArgs e)
+        {
             TextBlock.Text = TextBox.Text;
             GoToUneditableMode();
         }
 
-        private void GoToEditableMode() {
+        private void GoToEditableMode()
+        {
             var da = CustomAnimation.GetEnteringScreenAnimation(0, 45, false);
             StackPanel_Bottom.BeginAnimation(HeightProperty, da);
             TextBox.Visibility = Visible;
@@ -36,7 +46,8 @@ namespace HCI___Assignment_Prototype.CustomControl {
             Button_Pencil.Visibility = Hidden;
         }
 
-        private void GoToUneditableMode() {
+        private void GoToUneditableMode()
+        {
             var da = CustomAnimation.GetLeavingScreenAnimation(45, 0, false);
             StackPanel_Bottom.BeginAnimation(HeightProperty, da);
             TextBox.Visibility = Collapsed;
@@ -47,8 +58,9 @@ namespace HCI___Assignment_Prototype.CustomControl {
 
         #region LabelProperty
 
-        public string Label {
-            get => (string) GetValue(LabelProperty);
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
             set => SetValue(LabelProperty, value);
         }
 
@@ -58,17 +70,19 @@ namespace HCI___Assignment_Prototype.CustomControl {
                 new PropertyMetadata("Label", LabelPropertyChangedCallback));
 
         private static void LabelPropertyChangedCallback(DependencyObject dependencyObject,
-                                                         DependencyPropertyChangedEventArgs e) {
+                                                         DependencyPropertyChangedEventArgs e)
+        {
             var d = dependencyObject as TextboxWithPencil;
-            d.TextBlock_Label.Text = (string) e.NewValue;
+            d.TextBlock_Label.Text = (string)e.NewValue;
         }
 
         #endregion
 
         #region TextProperty
 
-        public string Text {
-            get => (string) GetValue(TextProperty);
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
             set => SetValue(TextProperty, value);
         }
 
@@ -79,13 +93,56 @@ namespace HCI___Assignment_Prototype.CustomControl {
 
         private static void TextPropertyChangedCallback(DependencyObject dependencyObject,
                                                         DependencyPropertyChangedEventArgs
-                                                            dependencyPropertyChangedEventArgs) {
+                                                            dependencyPropertyChangedEventArgs)
+        {
             var d = dependencyObject as TextboxWithPencil;
-            var newValue = (string) dependencyPropertyChangedEventArgs.NewValue;
+            var newValue = (string)dependencyPropertyChangedEventArgs.NewValue;
+            if(d.IsPassword == true)
+            {
+                newValue = ToSecure(newValue);
+            }
             d.TextBox.Text = newValue;
             d.TextBlock.Text = newValue;
         }
 
         #endregion
+
+        #region IsPasswordProperty
+
+
+
+        public Boolean IsPassword
+        {
+            get { return (Boolean)GetValue(IsPasswordProperty); }
+            set { SetValue(IsPasswordProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsPassword.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPasswordProperty =
+            DependencyProperty.Register("IsPassword", typeof(Boolean), typeof(TextboxWithPencil), new PropertyMetadata(false));        
+
+        #endregion
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (IsPassword == false)
+                return;
+            TextBox.Text = ToSecure(TextBox.Text);
+        }
+
+        private static String ToSecure(String s)
+        {
+            var sb = new StringBuilder(s);
+            for (int i = 0; i < s.Length; i++)
+            {
+                sb[i] = '*';
+            }
+            return sb.ToString();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextPropertyChangedCallback(this, new DependencyPropertyChangedEventArgs(TextProperty, null, Text));
+        }
     }
 }
