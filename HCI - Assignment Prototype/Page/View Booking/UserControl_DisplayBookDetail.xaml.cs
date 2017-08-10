250b3c84145edcc9f55b39c9eedd04f2acb27c1d
yihan <faster960128@hotmail.com>
2017-08-10 19:46:01 +0800

    Adding feature display booking detail

--- HCI - Assignment Prototype/Page/View Booking/UserControl_DisplayBookDetail.xaml.cs
+++ HCI - Assignment Prototype/Page/View Booking/UserControl_DisplayBookDetail.xaml.cs
@@ -25,18 +25,25 @@ namespace HCI___Assignment_Prototype.Page.View_Booking
         public UserControl_DisplayBookDetail()
         {
             InitializeComponent();
+            //Count food and drink
             double ticketTotal = Global.Global.MovieReservation.NormalSeat.Split(' ').Length * 15.9;
             double comboATotal = Global.Global.MovieReservation.FoodAndDrinks[0].Quantity * Global.Global.MovieReservation.FoodAndDrinks[0].Price;
             double comboBTotal = Global.Global.MovieReservation.FoodAndDrinks[1].Quantity * Global.Global.MovieReservation.FoodAndDrinks[1].Price;
             double comboCTotal = Global.Global.MovieReservation.FoodAndDrinks[2].Quantity * Global.Global.MovieReservation.FoodAndDrinks[2].Price;
             double comboDTotal = Global.Global.MovieReservation.FoodAndDrinks[3].Quantity * Global.Global.MovieReservation.FoodAndDrinks[3].Price;
             double TotalPayment = ticketTotal + comboATotal + comboBTotal + comboCTotal + comboDTotal;
-
+            
             ticketQuantity.Content = Global.Global.MovieReservation.NormalSeat.Split(' ').Length;
             ComboAQuantity.Content = Global.Global.MovieReservation.FoodAndDrinks[0].Quantity;
             ComboBQuantity.Content = Global.Global.MovieReservation.FoodAndDrinks[1].Quantity;
             ComboCQuantity.Content = Global.Global.MovieReservation.FoodAndDrinks[2].Quantity;
             ComboDQuantity.Content = Global.Global.MovieReservation.FoodAndDrinks[3].Quantity;
+
+            TicketPrice.Content = "RM 14.90";
+            ComboAPrice.Content = Global.Global.MovieReservation.FoodAndDrinks[0].Price;
+            ComboBPrice.Content = Global.Global.MovieReservation.FoodAndDrinks[1].Price;
+            ComboCPrice.Content = Global.Global.MovieReservation.FoodAndDrinks[2].Price;
+            ComboDPrice.Content = Global.Global.MovieReservation.FoodAndDrinks[3].Price;
 
             TicketTotal.Content = "RM " + (Global.Global.MovieReservation.NormalSeat.Split(' ').Length * 15.9).ToString();
             ComboATotal.Content = "RM " + Global.Global.MovieReservation.FoodAndDrinks[0].Quantity * Global.Global.MovieReservation.FoodAndDrinks[0].Price;
@@ -45,6 +52,8 @@ namespace HCI___Assignment_Prototype.Page.View_Booking
             ComboDTotal.Content = "RM " + Global.Global.MovieReservation.FoodAndDrinks[3].Quantity * Global.Global.MovieReservation.FoodAndDrinks[3].Price;
             Total.Content = "RM " + TotalPayment;
 
+            string comboes =null;
+
             for (int i = 0; i < Global.Global.MovieReservation.FoodAndDrinks.Count; i++)
             {
                 if(Global.Global.MovieReservation.FoodAndDrinks[i].Quantity == 0)
@@ -52,30 +61,47 @@ namespace HCI___Assignment_Prototype.Page.View_Booking
                     if(i == 0)
                     {
                         LabelA.Visibility = Visibility.Collapsed;
+                        ComboAPrice.Visibility = Visibility.Collapsed;
                         ComboAQuantity.Visibility = Visibility.Collapsed;
                         ComboATotal.Visibility = Visibility.Collapsed;
                     }
                     else if(i == 1)
                     {
                         LabelB.Visibility = Visibility.Collapsed;
+                        ComboBPrice.Visibility = Visibility.Collapsed;
                         ComboBQuantity.Visibility = Visibility.Collapsed;
                         ComboBTotal.Visibility = Visibility.Collapsed;
                     }
                     else if(i == 2)
                     {
                         LabelC.Visibility = Visibility.Collapsed;
+                        ComboCPrice.Visibility = Visibility.Collapsed;
                         ComboCQuantity.Visibility = Visibility.Collapsed;
                         ComboCTotal.Visibility = Visibility.Collapsed;
                     }
                     else if(i == 3)
                     {
                         LabelD.Visibility = Visibility.Collapsed;
+                        ComboDPrice.Visibility = Visibility.Collapsed;
                         ComboDQuantity.Visibility = Visibility.Collapsed;
                         ComboDTotal.Visibility = Visibility.Collapsed;
                     }
                 }
+                if(Global.Global.MovieReservation.FoodAndDrinks[i].Quantity != 0)
+                {
+                    comboes += Global.Global.MovieReservation.FoodAndDrinks[i].Name + "(" + Global.Global.MovieReservation.FoodAndDrinks[i].Quantity + ") \n";
+                }
             }
-            
+
+            //display reservation detail
+
+            Movie.Content = Global.Global.MovieReservation.MovieName;
+            Location.Content = Global.Global.MovieReservation.Location;
+            Date.Content = Global.Global.MovieReservation.Date;
+            Time.Content = Global.Global.MovieReservation.Time;
+            Seat.Content = Global.Global.MovieReservation.NormalSeat;
+
+            Combo.Content = comboes;
         }
 
         private void Click_cancelDetail(object sender, RoutedEventArgs e)
