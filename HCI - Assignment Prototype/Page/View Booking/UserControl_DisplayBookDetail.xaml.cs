d25239d22f9a0a768c7253b921709aac7f2ee09b
wongjiahau <hou32hou@gmail.com>
2017-08-10 19:44:20 +0800

    Fix Issue#3 - customer paying extraordinary amount

--- HCI - Assignment Prototype/Page/View Booking/UserControl_DisplayBookDetail.xaml.cs
+++ HCI - Assignment Prototype/Page/View Booking/UserControl_DisplayBookDetail.xaml.cs
@@ -15,81 +15,75 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HCI___Assignment_Prototype.Page.View_Booking
-{
+namespace HCI___Assignment_Prototype.Page.View_Booking {
     /// <summary>
     /// Interaction logic for UserControl_DisplayBookDetail.xaml
     /// </summary>
-    public partial class UserControl_DisplayBookDetail : UserControl
-    {
-        public UserControl_DisplayBookDetail()
-        {
+    public partial class UserControl_DisplayBookDetail : UserControl {
+        public UserControl_DisplayBookDetail() {
             InitializeComponent();
-            double ticketTotal = Global.Global.MovieReservation.NormalSeat.Split(' ').Length * 15.9;
-            double comboATotal = Global.Global.MovieReservation.FoodAndDrinks[0].Quantity * Global.Global.MovieReservation.FoodAndDrinks[0].Price;
-            double comboBTotal = Global.Global.MovieReservation.FoodAndDrinks[1].Quantity * Global.Global.MovieReservation.FoodAndDrinks[1].Price;
-            double comboCTotal = Global.Global.MovieReservation.FoodAndDrinks[2].Quantity * Global.Global.MovieReservation.FoodAndDrinks[2].Price;
-            double comboDTotal = Global.Global.MovieReservation.FoodAndDrinks[3].Quantity * Global.Global.MovieReservation.FoodAndDrinks[3].Price;
+            int _ticketQuantity = Global.Global.MovieReservation.NormalSeat.Trim().Split(' ').Length;
+            int comboAQuantity = Global.Global.MovieReservation.FoodAndDrinks[0].Quantity;
+            int comboBQuantity = Global.Global.MovieReservation.FoodAndDrinks[1].Quantity;
+            int comboCQuantity = Global.Global.MovieReservation.FoodAndDrinks[2].Quantity;
+            int comboDQuantity = Global.Global.MovieReservation.FoodAndDrinks[3].Quantity;
+
+            double ticketTotal = _ticketQuantity * 15.9;
+            double comboATotal = comboAQuantity * Global.Global.MovieReservation.FoodAndDrinks[0].Price;
+            double comboBTotal = comboBQuantity * Global.Global.MovieReservation.FoodAndDrinks[1].Price;
+            double comboCTotal = comboCQuantity * Global.Global.MovieReservation.FoodAndDrinks[2].Price;
+            double comboDTotal = comboDQuantity * Global.Global.MovieReservation.FoodAndDrinks[3].Price;
             double TotalPayment = ticketTotal + comboATotal + comboBTotal + comboCTotal + comboDTotal;
 
-            ticketQuantity.Content = Global.Global.MovieReservation.NormalSeat.Split(' ').Length;
-            ComboAQuantity.Content = Global.Global.MovieReservation.FoodAndDrinks[0].Quantity;
-            ComboBQuantity.Content = Global.Global.MovieReservation.FoodAndDrinks[1].Quantity;
-            ComboCQuantity.Content = Global.Global.MovieReservation.FoodAndDrinks[2].Quantity;
-            ComboDQuantity.Content = Global.Global.MovieReservation.FoodAndDrinks[3].Quantity;
+            ticketQuantity.Content = _ticketQuantity;
+            ComboAQuantity.Content = comboAQuantity;
+            ComboBQuantity.Content =comboBQuantity;
+            ComboCQuantity.Content = comboCQuantity;
+            ComboDQuantity.Content = comboDQuantity;
 
-            TicketTotal.Content = "RM " + (Global.Global.MovieReservation.NormalSeat.Split(' ').Length * 15.9).ToString();
-            ComboATotal.Content = "RM " + Global.Global.MovieReservation.FoodAndDrinks[0].Quantity * Global.Global.MovieReservation.FoodAndDrinks[0].Price;
-            ComboBTotal.Content = "RM " + Global.Global.MovieReservation.FoodAndDrinks[1].Quantity * Global.Global.MovieReservation.FoodAndDrinks[1].Price;
-            ComboCTotal.Content = "RM " + Global.Global.MovieReservation.FoodAndDrinks[2].Quantity * Global.Global.MovieReservation.FoodAndDrinks[2].Price;
-            ComboDTotal.Content = "RM " + Global.Global.MovieReservation.FoodAndDrinks[3].Quantity * Global.Global.MovieReservation.FoodAndDrinks[3].Price;
+            TicketTotal.Content = "RM " + ticketTotal;
+            ComboATotal.Content = "RM " + comboATotal; 
+            ComboBTotal.Content = "RM " + comboBTotal; 
+            ComboCTotal.Content = "RM " + comboCTotal; 
+            ComboDTotal.Content = "RM " + comboDTotal; 
             Total.Content = "RM " + TotalPayment;
 
-            for (int i = 0; i < Global.Global.MovieReservation.FoodAndDrinks.Count; i++)
-            {
-                if(Global.Global.MovieReservation.FoodAndDrinks[i].Quantity == 0)
-                {
-                    if(i == 0)
-                    {
+            for (int i = 0 ; i < Global.Global.MovieReservation.FoodAndDrinks.Count ; i++) {
+                if (Global.Global.MovieReservation.FoodAndDrinks[i].Quantity == 0) {
+                    if (i == 0) {
                         LabelA.Visibility = Visibility.Collapsed;
                         ComboAQuantity.Visibility = Visibility.Collapsed;
                         ComboATotal.Visibility = Visibility.Collapsed;
                     }
-                    else if(i == 1)
-                    {
+                    else if (i == 1) {
                         LabelB.Visibility = Visibility.Collapsed;
                         ComboBQuantity.Visibility = Visibility.Collapsed;
                         ComboBTotal.Visibility = Visibility.Collapsed;
                     }
-                    else if(i == 2)
-                    {
+                    else if (i == 2) {
                         LabelC.Visibility = Visibility.Collapsed;
                         ComboCQuantity.Visibility = Visibility.Collapsed;
                         ComboCTotal.Visibility = Visibility.Collapsed;
                     }
-                    else if(i == 3)
-                    {
+                    else if (i == 3) {
                         LabelD.Visibility = Visibility.Collapsed;
                         ComboDQuantity.Visibility = Visibility.Collapsed;
                         ComboDTotal.Visibility = Visibility.Collapsed;
                     }
                 }
             }
-            
+
         }
 
-        private void Click_cancelDetail(object sender, RoutedEventArgs e)
-        {
+        private void Click_cancelDetail(object sender , RoutedEventArgs e) {
             MainWindow.MainFrame.GoBack();
-            
+
         }
 
-        private void Click_ConfirmBooking(object sender, RoutedEventArgs e)
-        {
-            ProgressDialog.Show("Making Reservation . . .", "",
-                () =>
-                {
-                    DialogBox.Show("Payment Successful", "Your Reservation is already made, your e-tickets are sent to your e-mail.", "OK");
+        private void Click_ConfirmBooking(object sender , RoutedEventArgs e) {
+            ProgressDialog.Show("Making Reservation . . ." , "" ,
+                () => {
+                    DialogBox.Show("Payment Successful" , "Your Reservation is already made, your e-tickets are sent to your e-mail." , "OK");
                     Global.Global.CurrentUser.Reservations.Add(Global.Global.MovieReservation);
                 });
 
