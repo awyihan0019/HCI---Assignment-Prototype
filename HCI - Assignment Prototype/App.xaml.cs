531975dc1701ac6eb8662b9a8ee79ca7b62e1505
WJH <hou32hou@gmail.com>
2017-08-11 09:42:39 +0800

    Fix a not-shutting-down-properly bug

--- HCI - Assignment Prototype/App.xaml.cs
+++ HCI - Assignment Prototype/App.xaml.cs
@@ -4,5 +4,9 @@ namespace HCI___Assignment_Prototype {
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application { }
+    public partial class App : Application {
+        private void App_OnExit(object sender, ExitEventArgs e) {
+            Application.Current.Shutdown();
+        }
+    }
 }
